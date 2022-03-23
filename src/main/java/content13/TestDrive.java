775ce595c5502a8955ad703aafaa063f6f3e714a
frknsnmz <furkan.sonmez@bilgiedu.net>
2022-03-24 00:04:26 +0300

    Content13

---
+++ src/main/java/content13/TestDrive.java
@@ -0,0 +1,29 @@
+package content13;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class TestDrive {
+
+    public static void exec(List<String> n, List<Integer> a) {
+        Iterator<String> i = n.iterator();
+        while(i.hasNext()) {
+            System.out.println(" --> " + i.next());
+        }
+
+        Iterator<Integer> i2 = a.iterator();
+        while(i.hasNext()) {
+            System.out.println(" --> " + i2.next());
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        List<String> names = List.of("James Hetfield", "Kirk Ham.", "Robert", "Lars", "Cliff");
+        List<Integer> ages = List.of(51, 50, 40, 54, 35);
+
+        //exec(names,ages);
+        // exec2(names,ages);
+    }
+}
