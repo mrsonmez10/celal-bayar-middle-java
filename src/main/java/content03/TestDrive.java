4866306320f2fb8f3a8ec0206237d609e5c2bc26
frknsnmz <furkan.sonmez@bilgiedu.net>
2022-03-23 23:39:05 +0300

    Content03

---
+++ src/main/java/content03/TestDrive.java
@@ -0,0 +1,9 @@
+package content03;
+
+public class TestDrive {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/content03/ToplamaUtil.java
@@ -0,0 +1,13 @@
+package content03;
+
+public final class ToplamaUtil {
+
+    private ToplamaUtil() {
+
+    }
+
+    public static int topla(int a, int b) {
+        return a+b;
+    }
+
+}
