bb095fbf7f43a9b6612601e3e69f331b23579a4f
frknsnmz <furkan.sonmez@bilgiedu.net>
2022-03-23 23:55:45 +0300

    Content08

---
+++ src/main/java/content08/Metallica.java
@@ -0,0 +1,37 @@
+package content08;
+
+public class Metallica {
+
+    // 1-> Doğrudan erişilebilirliği kes
+    // 2-> verilerimde kontrol sağlıyorum.
+    // 3-> Read only
+    // 4-> write only
+
+    private String grupAdı;
+    private int grupUyeSayısı;
+
+    public Metallica(String grupAdı, int grupUyeSayısı) {
+        super();
+        this.grupAdı = grupAdı;
+        this.grupUyeSayısı = grupUyeSayısı;
+    }
+
+    public String getGrupAdı() {
+        return grupAdı;
+    }
+
+    public void setGrupAdı(String grupAdı) {
+        this.grupAdı = grupAdı;
+    }
+
+    public int getGrupUyeSayısı() {
+        return grupUyeSayısı;
+    }
+
+    public void setGrupUyeSayısı(int grupUyeSayısı) {
+        if(grupUyeSayısı < 0) {
+            System.out.println("olmaz");
+        }
+        this.grupUyeSayısı = grupUyeSayısı;
+    }
+}
---
+++ src/main/java/content08/TestDrive.java
@@ -0,0 +1,9 @@
+package content08;
+
+public class TestDrive {
+
+    public static void main(String[] args) {
+        Metallica james = new Metallica("Metallica", 4);
+        james.setGrupUyeSayısı(-35);
+    }
+}
