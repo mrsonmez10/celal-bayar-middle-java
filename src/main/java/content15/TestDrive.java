ce79d9a2dc52053990068df886c3de7831e116e9
frknsnmz <furkan.sonmez@bilgiedu.net>
2022-03-24 00:06:14 +0300

    Content15

---
+++ src/main/java/content15/TestDrive.java
@@ -0,0 +1,15 @@
+package content15;
+
+//What does this program do? - Page 274
+public class TestDrive {
+
+    static Integer i;
+    static int i2;
+    static Boolean b;
+    static boolean b2;
+
+    public static void main(String[] args) {
+        System.out.println(b2);
+        System.out.println(i2);
+    }
+}
