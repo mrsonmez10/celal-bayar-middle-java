77a61551fb197928cb880abb6b4cfeb511910607
frknsnmz <furkan.sonmez@bilgiedu.net>
2022-03-23 23:40:20 +0300

    Content04

---
+++ src/main/java/content04/TestDrive.java
@@ -0,0 +1,22 @@
+package content04;
+
+public class TestDrive {
+
+	// Burada bir sıkıntı var mı?
+	private static long topla() {
+		Long sum = 0L;
+		for (long i = 0; i < 10000; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		long startTime = System.nanoTime();
+		topla();
+		long endTime = System.nanoTime();
+		long totalTime = endTime - startTime;
+		System.out.println(totalTime);
+	}
+
+}
